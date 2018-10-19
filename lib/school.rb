efc92814a93a95e20493f988af3598464b3c7493
Duane Felton <dfelton2010@gmail.com>
2018-10-19 14:05:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,5 +15,12 @@ class School
    def grade(grade_level)
      roster[grade_level]
    end
-
+   
+   def sort
+     sorted_students = []
+     roster.each do |grade_level, students|
+       sorted_students[grade_level] = students.sort
+      end
+      sorted_students
+    end
 end
